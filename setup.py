9ca04ac8e937501bd70445e7689695f047bf4177
Charles Teinturier <teintu.c@gmail.com>
2016-03-05 19:51:18 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name='lsserial',
+    version='1.0',
+    description='List linux serial ports',
+    author='Charles Teinturier',
+    author_email='teintu.c@gmail.com',
+    scripts=['lsserial'],
+    install_requires=[
+        'pyserial'
+    ]
+)
